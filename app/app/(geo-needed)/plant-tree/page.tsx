9d17496d4f5b2a015903219ccdef30e9da085ca4
KunJon-analytics <johnadelakun1@gmail.com>
2025-01-14 16:13:36 +0100

    removed vercel geo from plant tree page

--- app/app/(geo-needed)/plant-tree/page.tsx
+++ app/app/(geo-needed)/plant-tree/page.tsx
@@ -1,18 +1,8 @@
-import { type Geo } from "@vercel/functions";
-
-import { env } from "@/env.mjs";
 import PlantTreeForm from "./_components/plant-tree-form";
 import TreeLocationContainer from "./_components/tree-location-container";
 import PlantTreeInfo from "./_components/plant-tree-info";
 
-export const dynamic = "force-dynamic";
-
 export default async function PlantTree() {
-  const data = await fetch(`${env.NEXT_PUBLIC_APP_URL}/api/my-location`);
-  const myLocation = (await data.json()) as Geo;
-
-  const { city, country } = myLocation;
-
   return (
     <>
       <div className="flex items-start">
@@ -21,14 +11,6 @@ export default async function PlantTree() {
 
       <TreeLocationContainer />
       <div className="text-center">
-        {city && country && (
-          <>
-            <p className="font-semibold">Your current location:</p>
-            <p>
-              {city}, {country}
-            </p>
-          </>
-        )}
         <p className="mt-4">
           Is this the exact location where you planted the tree?
         </p>
