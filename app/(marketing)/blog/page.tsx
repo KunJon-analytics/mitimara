b45bdb06f0a1e08abfbeeb1b40dfe554386b02fd
KunJon-analytics <johnadelakun1@gmail.com>
2025-01-21 18:40:29 +0100

    removed space from blog image src

--- app/(marketing)/blog/page.tsx
+++ app/(marketing)/blog/page.tsx
@@ -58,9 +58,7 @@ export default function Post() {
           <Timeline.Article
             key={post.slug}
             publishedAt={toDate(post.metadata.publishedAt)}
-            imageSrc={`/${
-              post.metadata.image ?? "assets/og/blog/default.png"
-            } `}
+            imageSrc={`/${post.metadata.image ?? "assets/og/blog/default.png"}`}
             title={post.metadata.title}
             href={`./blog/${post.slug}`}
           >
