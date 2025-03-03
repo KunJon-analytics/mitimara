260ca6d06001e9699c30597c726b46fa2533c5a5
KunJon-analytics <johnadelakun1@gmail.com>
2025-03-03 09:28:21 +0100

    changed pi key for prod

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -18,7 +18,6 @@ export default function GlobalError({
     <html lang="en">
       <body>
         {/* This is the default Next.js error component but it doesn't allow omitting the statusCode property yet. */}
-        {/* biome-ignore lint/suspicious/noExplicitAny: <explanation> */}
         <NextError statusCode={500} />
       </body>
     </html>
