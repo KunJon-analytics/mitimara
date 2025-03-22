b5a52cb4e12a2b89cbe74da346317467a409bfdc
KunJon-analytics <johnadelakun1@gmail.com>
2025-03-22 19:01:42 +0100

    fixed link

--- components/marketing/home/sliding-banner.tsx
+++ components/marketing/home/sliding-banner.tsx
@@ -9,10 +9,7 @@ export function SlidingBanner() {
     <div className="bg-green-100 p-4 rounded-lg shadow-md overflow-hidden h-16">
       <div className="relative h-full flex items-center">
         <div className="absolute animate-ticker whitespace-nowrap text-green-800">
-          <Link
-            className="hover:underline"
-            href={"/blog/mitimara-city-bounty-contests"}
-          >
+          <Link className="hover:underline" href={"/blog/introducing-mitimara"}>
             {announcement}
           </Link>
         </div>
